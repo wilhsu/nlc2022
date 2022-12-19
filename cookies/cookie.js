692032f2beef9e190ef378fdc17049e4ecaf0899
wilhsu <hsuw073@newschool.edu>
2022-12-19 01:50:57 +0100

    i tried

--- cookies/cookie.js
+++ cookies/cookie.js
@@ -1,9 +1,83 @@
-let cookiElement = document.getElementById("entry");
-let entryValue;
-console.log();
+document.cookie;
+var draggableElements = document.getElementsByClassName("drag");
 
-setCookie("thought", cookieElement, 999);
+for(var i = 0; i < draggableElements.length; i++){
+    dragElement(draggableElements[i]);
+}
 
+function dragElement(element) {
+    var pos1 = 0, pos2 = 0, pos3 = 0, pos4 = 0;
+    element.onmousedown = dragMouseDown;
+
+    function dragMouseDown(e) {
+        e = e || window.event;
+        e.preventDefault();
+        // get the mouse cursor position at startup:
+        pos3 = e.clientX;
+        pos4 = e.clientY;
+        document.onmouseup = closeDragElement;
+        // call a function whenever the cursor moves:
+        document.onmousemove = elementDrag;
+    }
+
+    function elementDrag(e) {
+        e = e || window.event;
+        e.preventDefault();
+        // calculate the new cursor position:
+        pos1 = pos3 - e.clientX;
+        pos2 = pos4 - e.clientY;
+        pos3 = e.clientX;
+        pos4 = e.clientY;
+        // set the element's new position:
+        element.style.top = (element.offsetTop - pos2) + "px";
+        element.style.left = (element.offsetLeft - pos1) + "px";
+    }
+
+    function closeDragElement() {
+        // stop moving when mouse button is released:
+        document.onmouseup = null;
+        document.onmousemove = null;
+    }
+}
+
+// let pigeonCookie = document.getElementById("peeeeegion");
+
+let pigeonCookie = document.querySelectorAll("div img");
+
+let size = parseInt(getCookie("pigeon_size")) || 300;
+
+for (let pigeon of pigeonCookie){
+    // console.log(getCookie(letter.id));
+    if (getCookie(pigeon.id) != ''){
+        // letter.style.textShadow = getCookie(letter.id);
+        console.log(getCookie(pigeon.id));
+        pigeon.style.width = getCookie(pigeon.id)
+        // letter.style.left = getCookie(pigeon.id + "_x");
+        // letter.style.top = getCookie(pigeon.id + "_y");
+    }
+
+    pigeon.style.width = size + "px";
+    pigeon.style.height = size + "px";
+
+    pigeon.addEventListener("mousedown",function(){
+        size+=10;
+        this.style.width = size + "px";
+        // this.style.height = size + "px";
+    
+        setCookie("pigeon_size", size, 999);
+    })
+}
+
+
+const deleteAllCookies = () => {
+    const cookies = document.cookie.split(";");
+  
+    for (const cookie of cookies) {
+      const eqPos = cookie.indexOf("=");
+      const name = eqPos > -1 ? cookie.substr(0, eqPos) : cookie;
+      document.cookie = name + "=;expires=Thu, 01 Jan 1970 00:00:00 GMT";
+    }
+}
 
 function setCookie(cname, cvalue, exdays) {
     const d = new Date();
@@ -27,6 +101,7 @@ function getCookie(cname) {
     }
     return "";
 }
+
 
 function checkCookie(cname) {
     if (!getCookie(cname) === undefined) {
